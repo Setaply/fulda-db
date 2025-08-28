a048caeb92e45e34821613ce86d548431720a1bf
Setaply <contact.setaply@gmail.com>
2025-08-28 13:30:18 +0200

    Init

--- config/admin.js
+++ config/admin.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: 'e24c3f8ec7e6b2cf03caaa5fdd4d2bbf7b14b5dc506c6c1921b98a645adce04d',
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: '7387f3901742f471f2b3bf082f6e6f72fdc0e7633ec3536276408941b2d32780',
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: '4644af1ae80531c2f8d3c06f9134c5832577e5019007ba41fd7b52c063065ee7',
     },
   },
   flags: {
--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: ['f233edc3eb1f1e1522384412d7d4f90675155bc86551aae0d1f186de269b6b45'],
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
